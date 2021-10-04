82e7f07d24e4388a1676c8145a2500b276eb8bd3
m-silver <mattsilver@windowslive.com>
2021-10-04 13:49:47 +1300

    Set up basic form

--- src/pages/registerCard/CardForm.tsx
+++ src/pages/registerCard/CardForm.tsx
@@ -1,8 +1,73 @@
+import { useState } from 'react'
+import {
+  Button,
+	TextField,
+  Grid
+} from '@mui/material'
+
+
 const CardForm = () => {
+  const [cardNumber, setCardNumber] = useState<string>('')
+  const [cvc, setCvc] = useState<string>('')
+  const [expiry, setExpiry] = useState<string>('')
+
+  const submitHandler = () => {
+    console.log('Submitted')
+  }
+
   return (
-    <>
-      CardForm
-    </>
+    <form className='cardForm' onSubmit={submitHandler}>
+      <Grid container direction='column' spacing={5}>
+        <Grid item>
+          <TextField
+            name='cardNumber'
+            label='Card Number'
+            type='text'
+            placeholder='1234 1234 1234 1234'
+            value={cardNumber}
+            onChange={e => setCardNumber(e.target.value)}
+            required
+            autoFocus
+            fullWidth
+          />
+        </Grid>
+
+        <Grid container item direction='row' spacing={3}>
+          <Grid item>
+            <TextField 
+
+              name='cvc'
+              label='CVC'
+              type='text'
+              placeholder='123'
+              value={cvc}
+              onChange={e => setCvc(e.target.value)}
+              required
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField 
+              name='expiry'
+              label='Expiry Date'
+              type='text'
+              placeholder='MM/YY'
+              value={expiry}
+              onChange={e => setExpiry(e.target.value)}
+              required
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item>    
+          <Button
+            type='submit' 
+            variant='outlined'>
+            Register Your Card
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   )
 }
 
