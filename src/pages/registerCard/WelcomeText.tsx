f2e0a02925ec53acce367f61c2d64c2128e33523
m-silver <mattsilver@windowslive.com>
2021-10-04 14:15:00 +1300

    Add styling to WelcomeText

--- src/pages/registerCard/WelcomeText.tsx
+++ src/pages/registerCard/WelcomeText.tsx
@@ -1,12 +1,14 @@
+import { Typography } from '@mui/material'
+
 type WelcomeTextProps = {
   user: User
 }
 
-const WelcomeText = ({ user }: WelcomeTextProps) => {
+const WelcomeText = ({user}: WelcomeTextProps) => {
   return (
-    <>
+    <Typography className='welcomeText' variant='h4'>
       Welcome {user.firstName}
-    </>
+    </Typography>
   )
 }
 
