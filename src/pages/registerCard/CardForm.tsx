31a7e80514d0dd36ea777fe5e2c9a4a6c0fa3fad
m-silver <mattsilver@windowslive.com>
2021-10-04 14:00:22 +1300

    Add validation to cardForm

--- src/pages/registerCard/CardForm.tsx
+++ src/pages/registerCard/CardForm.tsx
@@ -6,11 +6,38 @@ import {
   Grid
 } from '@mui/material'
 
+import { 
+  validateCardNumber,
+  validateCvc,
+  validateExpiry
+ } from '../../utils/validate'
+ import { formatExpiryDate } from '../../utils/utils'
 
 const CardForm = () => {
   const [cardNumber, setCardNumber] = useState<string>('')
+  const [cardNumberError, setCardNumberError] = useState<string>('')
   const [cvc, setCvc] = useState<string>('')
+  const [cvcError, setCvcError] = useState<string>('')
   const [expiry, setExpiry] = useState<string>('')
+  const [expiryError, setExpiryError] = useState<string>('')
+
+  const cardNumberBlur = () => {
+    setCardNumberError('')
+    const error = validateCardNumber(cardNumber)
+    if (error) setCardNumberError(error)
+  }
+
+  const cvcBlur = () => {
+    setCvcError('')
+    const error = validateCvc(cvc)
+    if (error) setCvcError(error)
+  }
+
+  const expiryBlur = () => {
+    setExpiryError('')
+    const error = validateExpiry(expiry)
+    if (error) setExpiryError(error)
+  }
 
   const submitHandler = () => {
     console.log('Submitted')
@@ -34,6 +61,9 @@ const CardForm = () => {
             required
             autoFocus
             fullWidth
+            onBlur={cardNumberBlur}
+            error={cardNumberError ? true : false}
+            helperText={cardNumberError}
           />
         </Grid>
 
@@ -51,6 +81,9 @@ const CardForm = () => {
                 setCvc(value)
               }}
               required
+              onBlur={cvcBlur}
+              error={cvcError ? true : false}
+              helperText={cvcError}
             />
           </Grid>
 
@@ -67,6 +100,9 @@ const CardForm = () => {
                 setExpiry(value)
               }}
               required
+              onBlur={expiryBlur}
+              error={expiryError ? true : false}
+              helperText={expiryError}
             />
           </Grid>
         </Grid>
