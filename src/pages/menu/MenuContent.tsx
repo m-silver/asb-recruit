cc198975384690b23a7a961b29cdfa6eea41ba1a
m-silver <mattsilver@windowslive.com>
2021-10-04 14:23:17 +1300

    Add Menu links

--- src/pages/menu/MenuContent.tsx
+++ src/pages/menu/MenuContent.tsx
@@ -1,8 +1,28 @@
+import {
+  MenuList,
+  MenuItem,
+  Typography
+} from '@mui/material'
+
 const MenuContent = () => {
+  const menuItems = [
+    'Change Password',
+    'Card PIN',
+    'Set Netcode Limit',
+    'Manage Devices',
+    'Logout'
+  ]
+
   return (
-    <>
-      Menu Content
-    </>
+    <MenuList>
+      {
+        menuItems.map(menuItem => (
+          <MenuItem key={menuItem}>
+            <Typography variant='h4' m={'1em'}>{menuItem}</Typography>
+          </MenuItem>
+        ))
+      }
+    </MenuList>
   )
 }
 
